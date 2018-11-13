9bd3188b4e97e73df30d3bc3b7d19c5f9342a541
BT_1N3_16@BKTECH.LOCAL <BT_1N3_16@BKTECH.LOCAL>
2018-11-13 10:17:11 -0500

    Initial commit

---
+++ src/Sort.java
@@ -0,0 +1,20 @@
+public class Sort {
+    public void BubbleSort(int[] arr)
+    {
+        int swaps = 0;
+        boolean swapped = true;
+        while(swapped)
+        {
+            for(int i = 0; i < arr.length - 1; i++)
+            {
+                
+            }
+        }
+    }
+    public void swap(int[] arr, int a, int b)
+    {
+        int temp = arr[a];
+        arr[a] = b;
+        arr[b] = temp;
+    }
+}
